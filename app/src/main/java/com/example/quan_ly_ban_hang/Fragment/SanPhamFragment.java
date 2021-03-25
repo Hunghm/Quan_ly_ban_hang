efe11e3ecb23624bbe886ca76867391fe3bfc0bb
Do Minh Duc <ducdmph12611@fpt.edu.vn>
2021-03-25 19:23:22 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable/ic_close.png

--- app/src/main/java/com/example/quan_ly_ban_hang/Fragment/SanPhamFragment.java
+++ app/src/main/java/com/example/quan_ly_ban_hang/Fragment/SanPhamFragment.java
@@ -1,5 +1,6 @@
 package com.example.quan_ly_ban_hang.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.quan_ly_ban_hang.ChucNang.SuaSanPhamActivity;
+import com.example.quan_ly_ban_hang.ChucNang.ThemSanPhamActivity;
 import com.example.quan_ly_ban_hang.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SanPhamFragment extends Fragment {
 
     RecyclerView recySanPham;
+    FloatingActionButton btnadd;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.san_pham_fragment,container,false);
         recySanPham = (RecyclerView) view.findViewById(R.id.recycler_view_san_pham);
+        btnadd = (FloatingActionButton) view.findViewById(R.id.btn_add);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ThemSanPhamActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
